df74c181a398b38022971db2b82d9b5a4519f66e
Dmitry <leon1861@yandex.ru>
2022-04-04 22:32:29 +0700

    Начало ДЗ!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Task 1
+        int i = 0;
+        while (i < 10) {
+            i++;
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (; i > 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        // Task 2
+        int friday = 6;
+        do {
+            System.out.println("Today is friday " + friday + " th. Need prepare report!");
+            friday = friday + 7;
+        } while (friday < 31);
+
+        // Task 3
+        int year = 0;
     }
 }
